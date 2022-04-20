8be940578540c4b8fe1b2122ef5070da6bf249b7
samrecile <recilesam@gmail.com>
2022-04-19 14:12:47 -1000

    router

--- reactApp/frontend/src/App.js
+++ reactApp/frontend/src/App.js
@@ -1,13 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header'
 import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={NotesListPage} />
+        <Route path="/note/:id" component={NotePage}/>>
+      </div>
+    </Router>
   );
 }
 
--- reactApp/frontend/src/pages/NotesListPage.js
+++ reactApp/frontend/src/pages/NotesListPage.js
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import Listitem from '../components/Listitem'
 const NotesListPage = () => {
-  return (
-    <div>Notes</div>
+    
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('/api/notes')
+        let data = await response.json()
+        setNotes(data)
+    }
+
+    return (
+    <div>
+        <div className='notes-list'>
+            {notes.map((note, index) => (
+                <Listitem key={index} note={note}/>
+            ))}
+        </div>
+    </div>
   )
 }
 
